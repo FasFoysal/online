af32771f8067b71d54f5536a3a20b708e54cf03d
FasFoysal <shounfoysal@gmail.com>
2021-11-30 22:49:41 +1100

    first commit

---
+++ fontawesome-free-5.15.4-web/Jquery  1/js/costom.js
@@ -0,0 +1,12 @@
+(function ($){
+
+    $(document) .ready (function (){
+
+        $('a') .click  (function () {
+          $ ('.box').removeClass('foysal') ;
+  
+ })
+
+    })
+
+  }) (jQuery)
---
+++ js/main.js
@@ -0,0 +1,14 @@
+(function($){
+$(document).ready(function(){
+      
+
+    $(".progres-bar").loading();
+
+
+      var mixer = mixitup('.bottom4');
+    
+
+     
+})
+
+})(jQuery)
